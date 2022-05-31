19cb586a0790181786b0ba817151cbec118ebbc5
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-31 20:32:18 -0300

    feat: setup inicial + componentes base

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import "./Navbar.css"
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div></div>
+            <div className="title">AgroVita</div>
+            <div></div>
+        </nav>
+    )
+}
+
+export { Navbar }
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import "./Home.css"
+
+function Home() {
+    return (
+        <div className="home-page">
+            <section>
+                <h1>Sua solução de emergência em produtos agrícolas</h1>
+            </section>
+        </div>
+    )
+}
+
+export { Home }
