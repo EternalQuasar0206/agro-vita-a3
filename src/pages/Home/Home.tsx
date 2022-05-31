ed1979926152c58ccee8eddee14b83aaf9b62272
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-05-31 20:55:37 -0300

    chore: atualizações da página home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,11 +4,19 @@ function Home() {
     return (
         <div className="home-page">
             <section>
-                <h1>Sua solução de emergência em produtos agrícolas</h1>
+                <h1 className="title">Sua solução de emergência em produtos agrícolas</h1>
                 <div>
                     <div className="item">
                         <img src="/img/icons/agricultura.png" />
                         <h3>Sustentável para a natureza</h3>
+                    </div>
+                    <div className="item">
+                        <img src="/img/icons/agricultura-de-colheita.png" />
+                        <h3>David indo almoçar</h3>
+                    </div>
+                    <div className="item">
+                        <img src="/img/icons/campo.png" />
+                        <h3>Roça ruim que só dá mato</h3>
                     </div>
                 </div>
             </section>
